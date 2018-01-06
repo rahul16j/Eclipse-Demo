b9c970f6f081d7f7d0aad4412092720df0378a00
rahul singh <rahul16j1990@gmail.com>
2018-01-06 09:36:02 +0530

    first commint in eclipse web project

---
+++ GitTest/src/come/rahul/Test.java
@@ -0,0 +1,13 @@
+package come.rahul;
+
+public class Test {
+
+	public void show(){
+		System.out.println("this is show method of test");
+	}
+	public static void main(String[] args) {
+		System.out.println("this is Rahul here");
+		Test2 t=new Test2();
+		t.show2();
+	}
+}
---
+++ GitTest/src/come/rahul/Test2.java
@@ -0,0 +1,9 @@
+package come.rahul;
+
+public class Test2 extends Test{
+       Test t2;   
+	public void show2(){
+		show();
+	}
+	
+}
